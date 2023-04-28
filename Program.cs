00d9f5293f74eb270c64d942212cfbd67a0f6187
Karol Basilico <116791261+basilicokarol@users.noreply.github.com>
2023-04-28 09:47:22 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using SQLite;
+
+
+
+
+Console.WriteLine("Hello, World!");
+//connessione al db
+SQLiteConnection cn1 = new SQLiteConnection("chinook.db");
+var tblArtists = cn1.Query<Artist>("select * from artists");
+Console.WriteLine($"in questa tabella ci sono {tblArtists.Count}record!");
+
+//--------------------------------------------
+//fine del main.... inizio della dichiarazione
+//--------------------------------------------
+
+public class Artist
+{
+public int ArtistId{ get; set; }
+//public string Name { get; set; }
+
+}
